f56aae3da72e43c9785f1222afed05c5a9ee15fa
Gerson Manuel Sugianto <manuelsugianto@gmail.com>
2025-05-14 23:00:12 +0700

    update navigation link text from 'EKSIS' to 'Laporan'

--- resources/views/front/layout/nav.blade.php
+++ resources/views/front/layout/nav.blade.php
@@ -81,7 +81,7 @@
                         </a>
                     </li>
                                         <li>
-                        <a href="{{route('eksis')}}" class="block py-2 pr-4 pl-3 text-gray-700 border-b uppercase md:hover:text-blue-700 font-semibold border-gray-100 hover:bg-blue-400 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 ">EKSIS</a>
+                        <a href="{{route('eksis')}}" class="block py-2 pr-4 pl-3 text-gray-700 border-b uppercase md:hover:text-blue-700 font-semibold border-gray-100 hover:bg-blue-400 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 ">Laporan</a>
                     </li>
                 </ul>
             </div>
